85123930bbe3304d7b0624ab31e58b687f36fb2e
ayushku634 <ayushiit634@gmail.com>
2024-12-28 01:01:13 +0530

    User Model Created

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
+const connectToDB = require('./db/db');
+connectToDB();
+
 app.use(cors());
 
 app.get('/', (req, res) => {
